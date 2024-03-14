1f32b956990e174feb90b036584ee32297341acf
haniefbunyiep <haniefburhan17@gmail.com>
2024-03-14 13:56:38 +0700

    build : initial commit

---
+++ src/app/home/page.jsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="text-3xl flex justify-center items-center h-screen">
+      Home Page
+    </div>
+  );
+}
---
+++ src/app/products/page.jsx
@@ -0,0 +1,7 @@
+export default function ProductsPage() {
+  return (
+    <div className="text-3xl flex justify-center items-center h-screen">
+      Products Page
+    </div>
+  );
+}
